90fc8d424e51b7af37cb8f810d5413fadb0e1591
DariusCatrina <darius.catrina@yahoo.com>
2023-01-03 06:02:53 -0500

    MMultiBART 1st commit still bug for 4 decoders

--- run_training.sh
+++ run_training.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-#SBATCH --partition=bhuwan
+#SBATCH --partition=nlplab
 #SBATCH --mem-per-cpu=128G
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
  
-torchrun --nnodes=1 --nproc_per_node=1 train_model.py --seq_len=4096 --bsz=5 --dataset_name=gov_report --number_of_decoders=1 --bart_seq_len=1024
+torchrun --nnodes=1 --nproc_per_node=1 train_model.py --seq_len=4096 --bsz=5 --dataset_name=gov_report --number_of_decoders=1 --bart_seq_len=1024 --decoder_length=512
